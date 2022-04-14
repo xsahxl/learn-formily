6cfbbea00f9e207ec2644935e396a9db322738d8
xsahxl <xsahxl@126.com>
2022-04-14 22:36:27 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { createForm, onFormValuesChange } from "@formily/core";
+import { FormProvider, FormConsumer, Field } from "@formily/react";
+import {
+  FormItem,
+  FormLayout,
+  Input,
+  FormButtonGroup,
+  Submit,
+} from "@formily/antd";
 
-function App() {
+const form = createForm({
+  effects:()=>{
+    onFormValuesChange(console.log)
+  }
+});
+
+
+const App =() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FormProvider form={form}>
+      <FormLayout layout="vertical">
+        <Field
+          name="input"
+          title="输入框"
+          required
+          // initialValue="Hello world"
+          decorator={[FormItem]}
+          component={[Input]}
+        />
+      </FormLayout>
+      <FormConsumer>
+        {() => (
+          <div
+            style={{
+              marginBottom: 20,
+              padding: 5,
+              border: "1px dashed #666",
+            }}
+          >
+            实时响应：{form.values.input}
+          </div>
+        )}
+      </FormConsumer>
+      <FormButtonGroup>
+        <Submit onSubmit={console.log}>提交</Submit>
+      </FormButtonGroup>
+    </FormProvider>
   );
-}
+};
 
-export default App;
+export default App
