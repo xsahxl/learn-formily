8f385183320fd17bb999ca647827d5a7e68fdf8b
xsahxl <xsahxl@126.com>
2022-04-15 18:30:14 +0800

    fix: 🐛 update index.js fix 浏览器刷新问题

--- src/App.js
+++ src/App.js
@@ -10,12 +10,6 @@ import {
 
 const form = createForm({
   effects:()=>{
-    // npm start 打开浏览器后。。输入值都是正常的。
-    /**
-     * 但是 在浏览器里刷新后就不行了，导致以下问题
-     * 如果initialValue传了值，导致无法正常输入
-     * 如果initialValue没有传值，input可以正常输入，但是没有触发onFormValuesChange方法
-     *  */
     onFormValuesChange(console.log);
   }
 });
